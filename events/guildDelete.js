1dc8ccbecc8c2585919b3d561922f587afe6acf2
GilloDaby <rafou14@hotmail.com>
2025-09-29 18:10:28 -0400

    asas

--- events/guildDelete.js
+++
@@ -1,9 +0,0 @@
-const db = require('../db');
-module.exports = async (guild) => {
-  try {
-    console.log(`👋 Removed from: ${guild.name} (${guild.id})`);
-    await db.query('DELETE FROM settings WHERE guildId = ?', [guild.id]);
-  } catch (err) {
-    console.error(`❌ Cleanup failed for ${guild.id}:`, err);
-  }
-};
